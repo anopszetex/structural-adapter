a5e9532ed255e79786cda081a0bf4aa4c5ce6144
Andre Luis <andre.1357.luis@gmail.com>
2022-06-08 22:51:55 -0300

    ✨ add a list of character entities
    get characters from xml

--- src/business/integrations/rickUSA.js
+++ src/business/integrations/rickUSA.js
@@ -1,5 +1,21 @@
+import axios from 'axios'
+import { parseStringPromise } from 'xml2js'
+import Character from '../../entities/character.js'
+
+const URL = ''
 export default class RickUSA {
   static async getCharactersFromXML () {
+    const { data } = await axios.get(URL)
+
+    const options = {
+      explicitRoot: false,
+      explicitArray: false
+    }
+
+    const { results: { element: results = [] } } = await parseStringPromise(data, options)
     
+    const defaultFormat = Array.isArray(results) ? results : [results]
+
+    return defaultFormat.map(char => new Character(char))
   }
 }
